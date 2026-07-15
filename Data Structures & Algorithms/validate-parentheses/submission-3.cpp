70d1f41989987f9f0d9026f904781f1568a0af53
dece076 <dece076@gmail.com>
2026-07-15 09:34:23 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack <char> st;
+        for (char ch : s){
+            if (ch=='('||ch=='{'||ch=='['){
+                st.push(ch);
+            }
+            else if (st.empty()) return false;
+            else if (ch==')'&&st.top()=='(') st.pop();
+            else if (ch=='}'&&st.top()=='{') st.pop();
+            else if (ch==']'&&st.top()=='[') st.pop();
+            else {
+                st.push(ch);
+                break;
+            }
+        }
+        if (st.empty())
+            return true;
+        else
+            return false;
+    }
+};
