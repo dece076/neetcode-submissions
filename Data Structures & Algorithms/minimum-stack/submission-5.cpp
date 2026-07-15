c51e64341c91713f979425203c02b077b5f474b4
dece076 <dece076@gmail.com>
2026-07-15 18:35:00 +0530

    Add: minimum-stack - submission-5

---
+++ Data Structures & Algorithms/minimum-stack/submission-5.cpp
@@ -0,0 +1,43 @@
+class MinStack {
+
+private:
+    stack <int> st;
+    stack <int> minst;
+    int minE;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if (!st.empty()&&val<=minE) {
+            minst.push(val);
+            minE=val;
+        }
+        else if(!st.empty()) minE=min(minE,val);
+        else{
+            minE=val;
+            minst.push(val);
+        } 
+        st.push(val);
+    }
+    
+    void pop() {
+        if (!st.empty()){
+            if(st.top()==minst.top()){
+                st.pop();
+                minst.pop();
+                //minE=minst.top();
+            }
+            else st.pop();
+        }
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minst.top();
+    }
+};
